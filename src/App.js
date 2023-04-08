a0295e137b74abbe633c4496cea0860da7725796
yxp2918 <2918369878@qq.com>
2023-04-08 22:12:32 +0800

    add 登录页

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import './App.css'
+// material UI 重置
+import './assets/css/normalize.css'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 import React from 'react'
 import ToDoBoard from './ToDoBoard'
 import Login from './components/Login'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<ToDoBoard />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<ToDoBoard />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </>
   )
 }
 
